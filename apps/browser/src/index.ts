89be311864b959f4414c02f724dbc6f41a604040
Henri Fardeau <fardeauhenri@gmail.com>
2023-10-16 14:15:43 +0200

    chore: fix export

--- apps/browser/src/index.ts
+++ apps/browser/src/index.ts
@@ -1,55 +1,6 @@
-import type { InitOptions, SendOptions } from './type';
+import Beacon from './Beacon';
 
-class Beacon {
-  private dsn: string;
+const beaconInstance = new Beacon();
 
-  private publicKey: string;
-
-  private appId: string;
-
-  private enable: boolean;
-
-  private debug: boolean;
-
-  constructor() {
-    this.dsn = 'https://api.beacon.fr';
-    this.publicKey = '';
-    this.appId = '';
-
-    this.debug = false;
-    this.enable = true;
-  }
-
-  public init({ publicKey, appId, dsn, enable, debug }: InitOptions): void {
-    this.publicKey = publicKey;
-    this.appId = appId;
-
-    if (dsn) {
-      this.dsn = dsn;
-    }
-
-    if (enable) {
-      this.enable = enable;
-    }
-
-    if (debug) {
-      this.debug = debug;
-    }
-  }
-
-  public async send({ formId, data }: SendOptions): Promise<void> {
-    console.info({
-      enable: this.enable,
-      debug: this.debug,
-      dsn: this.dsn,
-      publicKey: this.publicKey,
-      appId: this.appId,
-      formId,
-      data,
-    });
-
-    return Promise.resolve();
-  }
-}
-
-export default new Beacon();
+// eslint-disable-next-line import/prefer-default-export
+export { beaconInstance as Beacon };
